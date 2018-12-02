3efe596482ae607604febe031a8b147b5e70fb7b
Mateusz Majer <mmajer@w.gamp.zgora.pl>
2018-12-02 15:15:22 +0100

    Created Automapper config

--- Passenger.Core/Repositories/IUserRepository.cs
+++ Passenger.Core/Repositories/IUserRepository.cs
@@ -1,14 +1,16 @@
 using System;
+using System.Collections.Generic;
 using Passenger.Core.Domain;
 
 namespace Passenger.Core.Repositories
 {
     public interface IUserRepository
-    { // Try use ID :)
+    {
          User Get(string email);
          User Get(Guid ID);
          void Add(User user);
-         void Remove(User user); 
+         void Remove(User user);  // Guid ID
          void Update(User user);
+         IEnumerable<User> GetAll();
     }
 }
